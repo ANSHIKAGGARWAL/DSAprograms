757378ab5fcdf57fa69f23b5566d224101fa51f2
Anshika Aggarwal <121662708+ANSHIKAGGARWAL@users.noreply.github.com>
2023-01-25 23:56:49 +0530

    Create bubble_Sorting_using_function.cpp

---
+++ bubble_Sorting_using_function.cpp
@@ -0,0 +1,31 @@
+#include<iostream.h>
+// Declaring function name bubbleSort
+void bubbleSort(int);
+  
+//main body
+int mian() {
+  cout<<"\t ENTER INTEGERS\n";
+  int unsort[5],i;
+  
+  for(i=0;i<=4;i++)
+    cin>>unsort[i];
+  //Calling of function bubbleSort
+  bubbleSort(unsort);
+  return 0;
+}
+
+//Defining bubbleSort function
+void bubbleSort(int unsort[5]) {
+  int i,j,x,swap=;
+  //including swap bubbleSort becomes efficient algorithm
+  
+  for(i=0;i<4;i++)
+  {
+    swap=0;
+     for(j=0;j<4;j++)
+     {
+       if(unsort[j]>unsort[j+1];
+          {
+            
+          }
+  
